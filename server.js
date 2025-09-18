a987d2630c7ce419b6d6ed4c5c07126158995ba1
molinakSCSU <molinak4@southernct.edu>
2025-09-18 20:27:26 +0000

    edited server

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ const url = require('url');
 //call back function
 serveStatic = function (req, res) {
 	let fileName = '.' + url.parse(req.url).pathname;
-	res.writeHead(200, {'Content-Type': 'text/plain' });
-	res.write("Place holder where the data should be sent");
-	res.end();
+	
+	fs.readFile(fileName, function (err, data) {
+		if (err) {
+			res.writeHead(404, {'Content-Type': 'text/plain'});
+			res.write("error");
+			res.end();
+		} else {
+
+			res.writeHead(200, {'Content-Type': 'image/jpg' });
+       			res.write(data);
+       			res.end();
 }
- 
+});}
 const myserver = http.createServer(serveStatic); //create a server object
 myserver.listen(80, function() {console.log("Listening on port 80" )}); 
 
